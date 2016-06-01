7e68d1e1399f371a110285e1d8f8180528ca40f4
sdossettswift <sdossettswift@gmail.com>
2016-06-01 13:09:04 -0500

    solve issues 1-10

--- basics.rb
+++ basics.rb
@@ -1,32 +1,32 @@
 print "1. "
-puts 5 == 2 * 2
+puts 4 == 2 * 2
 
 print "2. "
-puts 3 > 4
+puts 5 > 4
 
 print "3. "
-puts (3 * 2) > 10
+puts (10 * 2) > 10
 
 print "4. "
-puts 2 == 3
+puts 3 == 3
 
 print "5. "
-puts (1 > 0) && (2 <= 1) == true
+puts (1 > 0) && (1 <= 1) == true
 
 print "6. "
-puts 2 != 2
+puts 3 != 2
 
 print "7. "
-puts (14 <= 10) || (2 >= 3) == true
+puts (14 <= 14) || (2 >= 3) == true
 
 print "8. "
-puts 10 % 3 == 0
+puts 12 % 3 == 0
 
 print "9. "
-puts 12 == 10 ** 2
+puts 100 == 10 ** 2
 
 print "10. "
-puts 1.6 == 1000 / (300 / 2)
+puts 6 == 1000 / (300 / 2)
 
 #I AM A COMMENT RUBY IGNORES ME BECAUSE MY LINE STARTED WITH A '#'
 #Use the following variable msg for questions 11 - 15
