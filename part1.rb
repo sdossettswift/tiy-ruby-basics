a6b56659f133f4d656f4e52ac9391938dc625fe9
sdossettswift <sdossettswift@gmail.com>
2016-06-02 00:27:32 -0500

    part 1: words from strings

---
+++ part1.rb
@@ -0,0 +1,16 @@
+# PART 1: Words from strings
+#
+# Create an array of the words in the sentence below
+# Find how many words have a length equal to 5
+# "Tacos must not have lettuce Only meat and cheese and salsa"
+string= "Tacos must not have lettuce Only meat and cheese and salsa"
+
+words = string.split (" ")
+
+counter = 0;
+words.each do |word|
+  if word.length == 5
+    counter += 1
+  end
+end
+puts counter
